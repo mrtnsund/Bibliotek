089865c36069c8a57df5b870f72b023ac64d2d72
Morten Sund <mrtnsund@gmail.com>
2019-11-01 14:47:52 +0100

    la til både erlest-knapp og slett-knapp

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let bibliotek = [];
 
 function Book(title, author, pages, read){
+    
         this.title = title;
         this.author = author;
         this.pages = pages;
@@ -18,39 +19,49 @@ function display(){
     for (i; i < bibliotek.length; i++){
         let element = document.createElement('div');
         element.classList.add("bok");
-
+        element.id=i;
         let tittel = document.createElement('div');
         let forfatter = document.createElement('div');
         let sidetall = document.createElement('div');
         let erLest = document.createElement('div');
+        let fjern = document.createElement('div');
+        erLest.style.backgroundColor = "rgba(255,0,0,0.4)";
+        erLest.innerHTML = "Er ikke lest";
 
         tittel.classList.add("tittelBok");
         forfatter.classList.add("forfatterBok")
         sidetall.classList.add("sideantallBok");
         erLest.classList.add("erLest");
-        erLest.id = i;
+        fjern.classList.add("fjern");
 
         tittel.innerHTML = bibliotek[i].title;
         forfatter.innerHTML = bibliotek[i].author;
         sidetall.innerHTML = bibliotek[i].pages;
+        fjern.innerHTML = "Fjern";
 
-        function erlestFunksjon() {
-            if(bibliotek[i].read === true){
-                erLest.style.backgroundColor = "rgba(60, 179, 113, 0.4)";
-                return "Er lest";
-            } else {
+        erLest.addEventListener('click', () => {
+            if(bibliotek[element.id].read === false){
+                bibliotek[element.id].read = true;
                 erLest.style.backgroundColor = "rgba(255,0,0,0.4)";
-                return "Er ikke lest"
+                erLest.innerHTML = "Er ikke lest";
+            } else {
+                bibliotek[element.id].read = false;
+                erLest.style.backgroundColor = "rgba(60, 179, 113, 0.4)";
+                erLest.innerHTML = "Er lest";
             }
-        }
-        erLest.innerHTML = erlestFunksjon();
-        
+            display();
+        });
+        fjern.addEventListener('click', () => {
+            bibliotek[element.id] = undefined;
+            biblioDiv.removeChild(element);
+            display();
+        });
 
         element.appendChild(tittel);
         element.appendChild(forfatter);
         element.appendChild(sidetall);
         element.appendChild(erLest);
-
+        element.appendChild(fjern);
         biblioDiv.appendChild(element);
     }
 };
@@ -60,19 +71,9 @@ leggTilKnapp.addEventListener('click', () => {
     let tittel = document.getElementById('tittel').value;
     let forfatter = document.getElementById('forfatter').value;
     let antallSider = document.getElementById('sider').value;
-    let newBook = new Book(tittel, forfatter, antallSider, true);
+    let newBook = new Book(tittel, forfatter, antallSider, false);
     bibliotek.push(newBook);
     display();
 });
 
-function endreLest(id){
-    if (bibliotek[id].read === true){
-        bibliotek[id].read = false;
-        console.log(bibliotek[id].info())
-    } else {
-        bibliotek[id].read = true;
-        console.log(bibliotek[id].info())
-
-    }
-}
 display();
